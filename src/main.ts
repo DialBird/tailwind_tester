c8004ce58b068c2d3bfab536824b344779e9a840
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-10-30 20:09:33 +0900

    use jit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from '@/router'
-import VeryHeavy2 from '@/components/VeryHeavy2.vue'
+// import VeryHeavy2 from '@/components/VeryHeavy2.vue'
 // import { createProvider } from './vue-apollo'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -16,7 +16,7 @@ console.log(uuidv4)
 Vue.config.productionTip = false
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.component('VeryHeavy2', VeryHeavy2)
+// Vue.component('VeryHeavy2', VeryHeavy2)
 
 new Vue({
   render: (h) => h(App),
